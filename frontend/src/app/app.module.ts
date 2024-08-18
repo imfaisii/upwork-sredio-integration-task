22bc889cd862e0f3b3e5670d9cdf3522d4865f80
Faisal Ashfaq <cfaysal099@gmail.com>
2024-08-18 14:10:04 +0100

    (feat): added tooltips

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -16,13 +16,20 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 import { RepositoriesGridComponent } from './components/repositories-grid/repositories-grid.component';
 import { LinkRendererComponent } from './components/link-renderer/link-renderer.component';
 import { MaterialCheckboxRendererComponent } from './components/material-checkbox-renderer/material-checkbox-renderer.component';
 
 @NgModule({
-  declarations: [AppComponent, IntegrationExpansionPanelComponent, RepositoriesGridComponent, LinkRendererComponent, MaterialCheckboxRendererComponent],
+  declarations: [
+    AppComponent,
+    IntegrationExpansionPanelComponent,
+    RepositoriesGridComponent,
+    LinkRendererComponent,
+    MaterialCheckboxRendererComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -30,12 +37,13 @@ import { MaterialCheckboxRendererComponent } from './components/material-checkbo
     FontAwesomeModule,
     HttpClientModule,
     AgGridModule,
-    
+
+    MatTooltipModule,
     MatCheckboxModule,
     MatExpansionModule,
     MatButtonModule,
     MatIconModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
